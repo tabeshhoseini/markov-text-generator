bb5e99087c94ff41e978eeacf38003876a94dfd2
Tabesh <tabeshhoseini85@gmail.com>
2026-04-30 13:11:31 +0330

    adding strip function and cleaning words

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
-words = []
+import unicodedata
 
-with open("sample.txt", "r") as file:
-    words = file.read().split()
+words = {}
+
+
+def clean_word(word):
+    puncs = {}
+    for line in open("sample.txt", "r"):
+        for char in line:
+            category = unicodedata.category(char)
+            if category.startswith("P"):
+                puncs[char] = 1
+
+    punctuation = "".join(puncs)
+
+    return word.strip(punctuation).lower()
+
+
+for line in open("sample.txt", "r"):
+    seq = line.replace("—", " ").split()
+    for word in seq:
+        clean_word(word)
+        words[word] = 1
+
 
 print(words)
